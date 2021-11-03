40d676c90f5752416bfad1c63c1a0c3552eeeaac
Alec Thomson <alecthomson0210@gmail.com>
2021-11-03 14:03:15 +0800

    Fixes

--- simulate.py
+++ simulate.py
@@ -18,11 +18,9 @@ import time
 import dask.dataframe as dd
 from IPython import embed
 
-np.random.seed(21092)
 
-# @dask.delayed(nout=2)
-def get_cat(cat):
-    np.random.seed(21092)
+def get_cat(cat, seed):
+    np.random.seed(seed)
     names = [
         "Source ID",
         "Cluster ID",
@@ -64,22 +62,19 @@ def get_cat(cat):
 
 
 @dask.delayed(nout=2)
-def get_rm_props(good_cat, sigmas=1):
-    np.random.seed(21092)
+def get_rm_props(good_cat, seed, sigmas=1):
+    np.random.seed(seed)
     phases = np.random.uniform(-np.pi / 2, np.pi / 2, size=len(good_cat))
     sigma_rms = np.random.normal(13, 15, size=len(good_cat)) * sigmas
     sigma_rms[sigma_rms < 0] = 0
     return phases, sigma_rms
 
 
-np.random.seed(21092)
-
-
 @dask.delayed(nout=2)
 def fitter(
-    i, row, freqs, lsq, skads_freqs, phases, sigma_rms, i_noise, q_noise, u_noise
+    i, row, freqs, lsq, skads_freqs, phases, sigma_rms, i_noise, q_noise, u_noise, seed
 ):
-    np.random.seed(21092)
+    np.random.seed(seed)
     sk_i = [
         10 ** row["log(I)_151_MHz"],
         10 ** row["log(I)_610_MHz"],
@@ -102,39 +97,48 @@ def fitter(
     return i_spectrum_noisy, qu_spectrum_noisy
 
 
-np.random.seed(21092)
-
-
 @dask.delayed(nout=2)
-def get_band(band):
-    np.random.seed(21092)
+def get_band(band, seed):
+    np.random.seed(seed)
     data = {
         "band_2": {
-            "freqs": np.linspace(1296e6, 1440e6, 144),
-            "sens": 28e-6 * np.sqrt(144),
+            "freqs": np.linspace(1296e6, 1440e6, 144)[20:],
+            # "sens": 28e-6 * np.sqrt(144), # Pilot: 270 µJy per channel - first 20 dropped
+            # "sens": 270e-6
+            "sens": 19e-6*np.sqrt(124) # Robust 0 WALLABY style
         },
         "band_1": {
             "freqs": np.linspace(800e6, 1087e6, 288),
-            "sens": 25e-6 * np.sqrt(288),
+            # "sens": 25e-6 * np.sqrt(288), # Pilot: 300µJy per channel / 17.6µJy av, calc: 18µJy
+            "sens": 300e-6
         },
         "split_band": {
             "freqs": np.hstack(
                 [np.linspace(840e6, 984e6, 144), np.linspace(1296e6, 1440e6, 144)]
             ),
-            "sens": 22e-6 * np.sqrt(288),
+            # "sens": 22e-6 * np.sqrt(288), 
+            # Calculator: 13.4µJy
+            "sens": 13.4e-6*np.sqrt(288)# WALLABY style Robust 0, Tsys/eta=75
         },
         "band_1_and_2": {
             "freqs": np.hstack(
                 [np.linspace(800e6, 1087e6, 288), np.linspace(1296e6, 1440e6, 144)]
             ),
-            "sens": 19e-6 * np.sqrt(432),
+            # "sens": 19e-6 * np.sqrt(432),
+            "sens": np.mean([300e-6, 19e-6*np.sqrt(124)])
+        },
+        "RACS": {
+        "freqs": np.linspace(744, 1032, 288),
+        # "sens": 19e-6 * np.sqrt(432),
+        "sens": 300e-6 * np.sqrt(288)
         },
     }
     return data[band]["freqs"], data[band]["sens"]
 
 
 @dask.delayed(nout=3)
-def make_noise(good_cat, freqs, sens):
+def make_noise(good_cat, freqs, sens, seed):
+    np.random.seed(seed)
     i_noise = np.random.normal(loc=0, scale=sens, size=(len(good_cat), len(freqs)))
     q_noise = np.random.normal(loc=0, scale=sens, size=(len(good_cat), len(freqs)))
     u_noise = np.random.normal(loc=0, scale=sens, size=(len(good_cat), len(freqs)))
@@ -168,9 +172,11 @@ def make_cat(good_cat, mDicts, mDict_cls, pas, sigmas):
     good_tab = Table.from_pandas(good_cat)
     syn_tab = Table({k: [dic[k] for dic in mDicts] for k in mDicts[0]})
     clean_tab = Table({k: [dic[k] for dic in mDict_cls] for k in mDict_cls[0]})
-    good_tab.add_column(pas, name='Pol_Angle')
-    good_tab.add_column(sigmas, name='Sigma_RM')
-    tab = hstack([good_tab,syn_tab,clean_tab], table_names=['INPUT','SYNTH','CLEAN'])
+    good_tab.add_column(pas, name="Pol_Angle")
+    good_tab.add_column(sigmas, name="Sigma_RM")
+    tab = hstack(
+        [good_tab, syn_tab, clean_tab], table_names=["INPUT", "SYNTH", "CLEAN"]
+    )
     return tab
 
 
@@ -179,50 +185,62 @@ rmclean = dask.delayed(do_RMclean_1D.run_rmclean, nout=2)
 
 
 def main(skads):
-    np.random.seed(21092)
+
     cluster = LocalCluster(n_workers=32, threads_per_worker=2)
     # cluster = LocalCluster()
     print(cluster)
     client = Client(cluster)
     print(client)
-    good_cat, skads_freqs = get_cat(
-        skads
-    )
-    phases, sigma_rms = get_rm_props(good_cat)
-    for band in ["band_2", "band_1", "split_band", "band_1_and_2"]:
-    # for band in ["band_2"]:
-        freqs, sens = get_band(band)
-        freqs = freqs.compute()
-        lsq = (2.998e8 / np.asarray(freqs)) ** 2
-        i_noise, q_noise, u_noise = make_noise(good_cat, freqs, sens)
+    for seed_i, seed in enumerate([21092, 6151215, 77777, 951022, 135786]):
+        # if seed_i <= 3:
+        #     continue
+        good_cat, skads_freqs = get_cat(skads, seed)
+        np.random.seed(seed)
+        phases, sigma_rms = get_rm_props(good_cat, seed)
+        for band in ["band_2", "band_1", "split_band", "band_1_and_2"]:
+            freqs, sens = get_band(band, seed)
+            freqs = freqs.compute()
+            lsq = (2.998e8 / np.asarray(freqs)) ** 2
+            i_noise, q_noise, u_noise = make_noise(good_cat, freqs, sens, seed)
 
-        mDicts, aDicts = [], []
-        mDict_cls, aDict_cls = [], []
-        for i, row in good_cat.iterrows():
-            i_spectrum_noisy, qu_spectrum_noisy = fitter(
-                i, row, freqs, lsq, skads_freqs, phases, sigma_rms, i_noise, q_noise, u_noise
-            )
-            data = make_data(freqs, i_spectrum_noisy, qu_spectrum_noisy, sens)
-            mDict, aDict = rmsynth(
-                data=data,
-                dPhi_radm2=10,
-                phiMax_radm2=2000,
-                polyOrd=-3,
-                fit_function="log",
-            )
-            mDicts.append(mDict)
-            aDicts.append(aDict)
-            mDict_cl, aDict_cl = rmclean(mDict, aDict, cutoff=-5)
-            mDict_cls.append(mDict_cl)
-            aDict_cls.append(aDict_cl)
-        tab = make_cat(good_cat, mDicts, mDict_cls, phases, sigma_rms)
-        # time.sleep(10)
-        tab = tab.compute()
-        tab.write(f"{band}_sim_cat.csv", format='pandas.csv')
+            mDicts, aDicts = [], []
+            mDict_cls, aDict_cls = [], []
+            for i, row in good_cat.iterrows():
+                i_spectrum_noisy, qu_spectrum_noisy = fitter(
+                    i,
+                    row,
+                    freqs,
+                    lsq,
+                    skads_freqs,
+                    phases,
+                    sigma_rms,
+                    i_noise,
+                    q_noise,
+                    u_noise,
+                    seed,
+                )
+                data = make_data(freqs, i_spectrum_noisy, qu_spectrum_noisy, sens)
+                mDict, aDict = rmsynth(
+                    data=data,
+                    dPhi_radm2=10,
+                    phiMax_radm2=2000,
+                    polyOrd=-3,
+                    fit_function="log",
+                )
+                mDicts.append(mDict)
+                aDicts.append(aDict)
+                mDict_cl, aDict_cl = rmclean(mDict, aDict, cutoff=-5)
+                mDict_cls.append(mDict_cl)
+                aDict_cls.append(aDict_cl)
+            tab = make_cat(good_cat, mDicts, mDict_cls, phases, sigma_rms)
+            # time.sleep(10)
+            tab = tab.compute()
+            tab.write(f"data/{band}_sim_cat_realisation_{seed_i}.csv", format="pandas.csv")
 
 
 def cli():
     import argparse
+
     # Help string to be shown using the -h option
 
     descStr = f"""
@@ -235,17 +253,13 @@ def cli():
     )
 
     parser.add_argument(
-        "skads",
-        metavar="skads",
-        type=str,
-        help="Catalogue containing SKADS data.",
+        "skads", metavar="skads", type=str, help="Catalogue containing SKADS data.",
     )
 
     args = parser.parse_args()
-    
-    main(
-        skads=args.skads
-    )
+
+    main(skads=args.skads)
+
 
 if __name__ == "__main__":
     cli()
